c6652fdbf8ef7135ddf63988cc405e015f6badff
rtxa <usertxa@gmail.com>
2022-05-05 07:43:06 -0300

    Modernize Vector3f
    - Use brace initializers
    - Do some format clean up

--- src/math/Vector3f.cpp
+++ src/math/Vector3f.cpp
@@ -1,14 +1,6 @@
-#include <cmath>
 #include "Vector3f.h"
 
-Vector3f::Vector3f() : Vector3f(0.0f, 0.0f, 0.0f) {
-}
-
-Vector3f::Vector3f(float x, float y, float z) {
-	this->x = x;
-	this->y = y;
-	this->z = z;
-}
+#include <cmath>
 
 void Vector3f::set(float x, float y, float z) {
 	this->x = x;
@@ -17,25 +9,25 @@ void Vector3f::set(float x, float y, float z) {
 }
 
 Vector3f Vector3f::operator + (const Vector3f& vector) const {
-	return Vector3f(x + vector.x, y + vector.y, z + vector.z);
+	return {x + vector.x, y + vector.y, z + vector.z};
 }
 
 Vector3f Vector3f::operator - (const Vector3f& vector) const {
-	return Vector3f(x - vector.x, y - vector.y, z - vector.z);
+	return {x - vector.x, y - vector.y, z - vector.z};
 }
 
 Vector3f Vector3f::operator * (float scale) const {
-	return Vector3f(x * scale, y * scale, z * scale);
+	return {x * scale, y * scale, z * scale};
 }
 
 Vector3f Vector3f::operator / (float scale) const {
-	return Vector3f(x / scale, y / scale, z / scale);
+	return {x / scale, y / scale, z / scale};
 }
 
 Vector3f Vector3f::crossProduct(const Vector3f& vector) const {
-	return Vector3f(y * vector.z - z * vector.y,
-					z * vector.x - x * vector.z,
-					x * vector.y - y * vector.x);
+	return {y * vector.z - z * vector.y,
+			z * vector.x - x * vector.z,
+			x * vector.y - y * vector.x};
 }
 
 float Vector3f::dotProduct(const Vector3f& vector) const {
@@ -47,7 +39,7 @@ float Vector3f::length() const {
 }
 
 Vector3f Vector3f::operator - () const {
-	return Vector3f(-x, -y, -z);
+	return {-x, -y, -z};
 }
 
 void Vector3f::opposite() {
@@ -57,7 +49,7 @@ void Vector3f::opposite() {
 Vector3f Vector3f::normalized() const {
 	const float distance = length();
 
-	return Vector3f(x / distance, y / distance, z / distance);
+	return {x / distance, y / distance, z / distance};
 }
 
 void Vector3f::normalize() {
@@ -66,11 +58,12 @@ void Vector3f::normalize() {
 }
 
 bool Vector3f::epsilonEquals(const Vector3f& vector, float epsilon) const {
-	if (fabs(vector.x - x) > epsilon)
+	if (fabs(vector.x - x) > epsilon) {
 		return false;
-	if (fabs(vector.y - y) > epsilon)
+	} if (fabs(vector.y - y) > epsilon) {
 		return false;
-	if (fabs(vector.z - z) > epsilon)
+	} if (fabs(vector.z - z) > epsilon) {
 		return false;
+	}
 	return true;
 }
--- src/math/Vector3f.h
+++ src/math/Vector3f.h
@@ -6,34 +6,29 @@ class Vector3f {
 public:
 	static constexpr float CompareEpsilon = 0.001f; // this should go in Constants.h
 
-	float x, y, z;
+	float x {0.0f};
+	float y {0.0f};
+	float z {0.0f};
 
-	Vector3f();
-
-	Vector3f(float x, float y, float z);
+	Vector3f() = default;
+	Vector3f(float x, float y, float z) : x{x}, y{y}, z{z} {};
 
 	void set(float x, float y, float z);
 
 	Vector3f operator + (const Vector3f& vector) const;
-
 	Vector3f operator - (const Vector3f& vector) const;
-
 	Vector3f operator * (float scale) const;
-
 	Vector3f operator / (float scale) const;
 
 	Vector3f crossProduct(const Vector3f& vector) const;
-
 	float dotProduct(const Vector3f& vector) const;
-
+	
 	float length() const;
 
 	Vector3f operator - () const;
-
 	void opposite();
 
 	Vector3f normalized() const;
-
 	void normalize();
 
 	bool epsilonEquals(const Vector3f& vector, float epsilon) const;
