ead9b2ac7d687ff01041dd6571c54f00a504755e
rtxa <usertxa@gmail.com>
2022-07-01 00:15:18 -0300

    Add simple test for Quake parser

---
+++ common/test/test_maps.cpp
@@ -0,0 +1,13 @@
+#include <doctest/doctest.h>
+
+#include <parser/QuakeParser.h>
+
+#include <iostream>
+#include <string>
+
+TEST_CASE("Test working maps") {
+    SUBCASE("Basic Room") {
+        QuakeMap map;
+        CHECK_NOTHROW(QuakeParser().processMap("test_data/basic_room.map", map));
+    }   
+}
